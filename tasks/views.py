26a64e8b7fe5249055bd1b8a5ef8f825c9d08aa6
Bhavya Bibi <bhavyabibi007@gmail.com>
2021-03-08 21:19:57 +0400

    items added in one by one

--- tasks/views.py
+++ tasks/views.py
@@ -7,9 +7,13 @@ from.forms import*
 # Create your views here.
 def index(request):
     tasks=Task.objects.all()
-    form=TaskForm(request.POST)
-    if form.is_vvalid():
-        form.save()
-    return redirect('/')
-    context={'tasks':tasks,'form':form}
+    form=TaskForm()
+
+
+    if request.method == "POST":
+        form=TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    context = {'tasks':tasks,'form':form}
     return render(request,'tasks/list.html',context)
