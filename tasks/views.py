e31b501e6b49b4e77fe78d098323c8d1cc659df0
Bhavya Bibi <bhavyabibi007@gmail.com>
2021-03-09 14:06:30 +0400

    style added in to doapp

--- tasks/views.py
+++ tasks/views.py
@@ -34,13 +34,12 @@ def updateTask(request, pk):
     context={'form':form}
     return render(request ,'tasks/update_task.html',context)
 
-def deleteTask(request,pk):
-      item=Task.objects.get(id=pk)
-      if request.method == "POST":
-          item.delete()
-          return redirect("/")
-    
+def deleteTask(request, pk):
+	item = Task.objects.get(id=pk)
 
-    
- context={'item':item}
-return render(request,'tasks/delete.html',context)
+	if request.method == 'POST':
+		item.delete()
+		return redirect('/')
+
+	context = {'item':item}
+	return render(request, 'tasks/delete.html', context)
